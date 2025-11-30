f6303824e5b9ecb7b7afb7f576df10080332818e
Dmitriy Bulygin <demetrius.storm@gmail.com>
2025-11-30 13:18:34 +0300

    Задание 2.1 - Login

--- app/src/main/kotlin/ru/otus/coroutineshomework/ui/login/LoginViewModel.kt
+++ app/src/main/kotlin/ru/otus/coroutineshomework/ui/login/LoginViewModel.kt
@@ -3,9 +3,13 @@ package ru.otus.coroutineshomework.ui.login
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import ru.otus.coroutineshomework.ui.login.data.Credentials
 
 class LoginViewModel : ViewModel() {
-
+    private val api = LoginApi()
     private val _state = MutableLiveData<LoginViewState>(LoginViewState.Login())
     val state: LiveData<LoginViewState> = _state
 
@@ -16,6 +20,15 @@ class LoginViewModel : ViewModel() {
      */
     fun login(name: String, password: String) {
         // TODO: Implement login
+        _state.value = LoginViewState.LoggingIn
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val user = api.login(Credentials(name, password))
+                _state.postValue(LoginViewState.Content(user))
+            } catch (e: Exception) {
+                _state.postValue(LoginViewState.Login(e))
+            }
+        }
     }
 
     /**
@@ -23,5 +36,10 @@ class LoginViewModel : ViewModel() {
      */
     fun logout() {
         // TODO: Implement logout
+        _state.value = LoginViewState.LoggingOut
+        viewModelScope.launch(Dispatchers.IO) {
+            api.logout()
+            _state.postValue(LoginViewState.Login())
+        }
     }
 }
